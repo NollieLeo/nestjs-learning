0ee804b67fbd156cfe279350d2a9bbbc6a73e259
翁恺敏 <736653759@qq.com>
2026-01-08 09:55:50 +0800

    init env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { RangeModule } from './range/range.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 
 @Module({
-  imports: [UserModule, RangeModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+      load: [() => dotenv.config({ path: '.env' })],
+    }),
+    UserModule,
+    RangeModule,
+  ],
   controllers: [],
   providers: [],
 })
