377dfcc488b81d25f14f81e666102fe20a333f7a
翁恺敏 <736653759@qq.com>
2026-01-10 20:27:05 +0800

    init mysql config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RangeModule } from './range/range.module';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 import * as Joi from 'joi';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 
@@ -17,8 +18,30 @@ const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
         NODE_ENV: Joi.valid()
           .valid('development', 'production')
           .default('development'),
-        DB_PROT: Joi.number().default(3306),
+        DB_PORT: Joi.number().default(3090),
+        DB_SYNC: Joi.boolean().default(false),
+        DB_HOST: Joi.string().ip(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_TYPE: Joi.string()
+          .valid('mysql', 'postgres', 'sqlite', 'mssql')
+          .default('mysql'),
       }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: configService.get('DB_SYNC'),
+        }) as TypeOrmModuleOptions,
     }),
     UserModule,
     RangeModule,
